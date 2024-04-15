229025f335536e369e16cf7dc4711c0f241a215c
Adrianns <adrianemedinam@gmail.com>
2024-04-15 16:23:14 -0300

    + init

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:clean_architecture_course/entities/user.dart';
+import 'package:clean_architecture_course/value_objects/email.dart';
+import 'package:clean_architecture_course/value_objects/password.dart';
+import 'package:flutter/material.dart';
+
+void main() {}
