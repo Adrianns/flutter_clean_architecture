52f062494c1f9963547845fa7191135ed9b2a863
Adrianns <adrianemedinam@gmail.com>
2024-04-23 01:59:35 -0300

    + contact list page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:clean_architecture_course/app/features/contacts/domain/usecases/create_contact_usecase.dart';
 import 'package:clean_architecture_course/app/features/contacts/domain/usecases/get_all_contact_usecase.dart';
 import 'package:clean_architecture_course/app/features/contacts/presentation/cubit/contacts_cubit.dart';
+import 'package:clean_architecture_course/app/features/contacts/presentation/pages/contact_list_page.dart';
 import 'package:clean_architecture_course/injection_container.dart' as di;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -30,69 +31,6 @@ class MyApp extends StatelessWidget {
                   )..getAllContacts()),
         ],
         child: const ContactListPage(),
-      ),
-    );
-  }
-}
-
-class ContactListPage extends StatelessWidget {
-  const ContactListPage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: const Text(
-          'Contact List',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: BlocBuilder<ContactsCubit, ContactsState>(
-        builder: (context, state) {
-          if (state is ContactsLoading || state is ContactsInitial) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is ContactsLoaded) {
-            final contacts = state.contacts;
-            if (contacts.isEmpty) {
-              return const Center(
-                child: Text('No Contacts'),
-              );
-            } else {
-              return ListView.separated(
-                itemCount: contacts.length,
-                itemBuilder: (context, index) {
-                  final contact = contacts[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      child: Text(
-                        contact.name.substring(0, 2),
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    title: Text(contact.name),
-                    subtitle: Text(contact.email.value),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const Divider();
-                },
-              );
-            }
-          } else if (state is ContactsError) {
-            return Center(
-              child: Text('Error: ${state.message}'),
-            );
-          } else {
-            return const SizedBox();
-          }
-        },
       ),
     );
   }
