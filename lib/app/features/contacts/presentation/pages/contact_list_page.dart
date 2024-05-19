34a8a9e2896394d24a149bb5f9c8d2154253b3e7
Pablo Martín Bazán <pablobazan6@gmail.com>
2024-05-18 21:38:00 -0300

    [Feature] Switch applied on contacts page (#2)

--- lib/app/features/contacts/presentation/pages/contact_list_page.dart
+++ lib/app/features/contacts/presentation/pages/contact_list_page.dart
@@ -1,5 +1,7 @@
+import 'package:clean_architecture_course/app/features/contacts/domain/entities/contact.dart';
 import 'package:clean_architecture_course/app/features/contacts/presentation/cubit/contacts_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// A page that displays a list of contacts.
@@ -18,50 +20,55 @@ class ContactListPage extends StatelessWidget {
       ),
       body: BlocBuilder<ContactsCubit, ContactsState>(
         builder: (context, state) {
-          if (state is ContactsLoading || state is ContactsInitial) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (state is ContactsLoaded) {
-            final contacts = state.contacts;
-            if (contacts.isEmpty) {
-              return const Center(
-                child: Text('No Contacts'),
-              );
-            } else {
-              return ListView.separated(
-                itemCount: contacts.length,
-                itemBuilder: (context, index) {
-                  final contact = contacts[index];
-                  return ListTile(
-                    leading: CircleAvatar(
-                      backgroundColor: Colors.black,
-                      child: Text(
-                        '${contact.name[0]}${contact.name[contact.name.indexOf(' ') + 1]}',
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    title: Text(contact.name),
-                    subtitle: Text(contact.email.value),
-                  );
-                },
-                separatorBuilder: (BuildContext context, int index) {
-                  return const Divider();
-                },
-              );
-            }
-          } else if (state is ContactsError) {
-            return Center(
-              child: Text('Error: ${state.message}'),
-            );
-          } else {
-            return const SizedBox();
-          }
+          return switch (state) {
+            ContactsLoading() || ContactsInitial() => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ContactsError() => Center(
+                child: Text('Error: ${state.message}'),
+              ),
+            ContactsLoaded() => _ContactsLoaded(state.contacts),
+          };
         },
       ),
+    );
+  }
+}
+
+class _ContactsLoaded extends StatelessWidget {
+  const _ContactsLoaded(this.contacts);
+
+  final List<Contact> contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    if (contacts.isEmpty) {
+      return const Center(
+        child: Text('No Contacts'),
+      );
+    }
+    return ListView.separated(
+      itemCount: contacts.length,
+      itemBuilder: (context, index) {
+        final contact = contacts[index];
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundColor: Colors.black,
+            child: Text(
+              '${contact.name[0]}${contact.name[contact.name.indexOf(' ') + 1]}',
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          title: Text(contact.name),
+          subtitle: Text(contact.email.value),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return const Divider();
+      },
     );
   }
 }
